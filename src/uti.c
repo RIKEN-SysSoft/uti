9ab4dfe7bf71df0b0bd19b7e246125ca3364c953
Masamichi Takagi <masamichi.takagi@riken.jp>
2018-05-23 17:04:49 +0900

    Run-time OS detection and branch (WIP)

--- src/uti.c
+++ src/uti.c
@@ -13,6 +13,7 @@
 #include <stdio.h>
 #include <strings.h>
 #include <stdbool.h>
+#include <errno.h>
 #include "uti.h"
 
 int uti_attr_init(uti_attr_t *attr) {
@@ -28,21 +29,62 @@ int uti_attr_destroy(uti_attr_t *attr) {
 int uti_pthread_create(pthread_t *thread, const pthread_attr_t * attr,
                        void *(*start_routine) (void *), void * arg, uti_attr_t *uti_attr) {
 	int rc;
-	char *disable = getenv("DISABLE_UTI");
+	char *disable_uti, *uti_os_kind;
 
-	if (!disable) {
+	disable_uti = getenv("DISABLE_UTI");
+	//printf("%s: enter,disable=%s\n", __FUNCTION__, disable_uti ? disable_uti : "NULL");
+
+	if (!disable_uti) {
 		rc = syscall(731, 1, uti_attr);
 		if (rc != 0) {
-			printf("%s: WARNING: util_indicate_clone is not available in this kernel\n");
-			disable = "1";
+			printf("%s: WARNING: util_indicate_clone is not available in this kernel\n", __FUNCTION__);
+			disable_uti = "1";
 		}
 	}
+
+#if 0
+	/* Allocation and scheduling using UTI_CPU_SET */
+
+	uti_os_kind = getenv("UTI_OS_KIND");
+	if (!uti_os_kind) {
+		uti_os_kind = "Linux";
+	}
+
+	if (!strcmp(uti_os_kind, "Linux")) {
+		char *uti_cpu_set_str;
+		uti_cpu_set_str = getenv("MY_ASYNC_PROGRESS_PIN");
+		if (!async_progress_pin_str) {
+			printf("%s: ERROR: MY_ASYNC_PROGRESS_PIN not found\n", __FUNCTION__);
+			goto sub_out;
+		}
+		
+		list = async_progress_pin_str;
+		while (1) {
+			token = strsep(&list, ",");
+			if (!token) {
+				break;
+			}
+			progress_cpus[n_progress_cpus++] = atoi(token);
+		}
+		
+		rank_str = getenv("PMI_RANK");
+		if (!rank_str) {
+			printf("%s: ERROR: PMI_RANK not found\n", __FUNCTION__);
+			goto sub_out;
+		}
+		rank = atoi(rank_str);
+
+		CPU_ZERO(&cpuset);
+		CPU_SET(progress_cpus[rank % n_progress_cpus], &cpuset);
+	}
+#endif
+
 	rc = pthread_create(thread, attr, start_routine, arg);
 	if (rc != 0) {
 		goto out;
 	}
 
-	if (!disable) {
+	if (!disable_uti) {
 		syscall(731, 0, NULL);
 	}
  out:
