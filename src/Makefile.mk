e8f7c25f9bfe505b5da7bcc3c578fa27ca4c821b
Masamichi Takagi <masamichi.takagi@riken.jp>
2018-11-07 14:31:20 +0900

    Makefile.am: Specify src/*.c and src/include/*.h in top Makefile.am

--- src/Makefile.mk
+++ src/Makefile.mk
@@ -1,12 +1,2 @@
-include_HEADERS += src/include/uti.h
-noinst_HEADERS += src/include/uti_impl.h
-
-AM_CPPFLAGS += \
-	-I$(top_srcdir)/src/include \
-	-I$(top_builddir)/src/include
-
-lib_lib@UTILIBNAME@_la_SOURCES += \
-	src/common.c
-
 include $(top_srcdir)/src/linux/Makefile.mk
 include $(top_srcdir)/src/mckernel/Makefile.mk
