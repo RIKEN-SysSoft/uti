b79584b71687784876d7749051f0c8e03b180a92
Masamichi Takagi <masamichi.takagi@riken.jp>
2018-11-19 17:15:38 +0900

    (squash me) Embed hwloc

--- autogen.sh
+++ autogen.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/bash
 
-# Fix the issue of missing AC_SUBST([MAKE]) in contrib/hwloc/configure.ac
-if ! grep -qE 'AC_CHECK_PROGS\(MAKE,make gnumake nmake pmake smake\)' contrib/hwloc/configure.ac; then
-    sed -i '/^AM_INIT_AUTOMAKE/ a if test "X$MAKE" = "X" ; then\n\tAC_CHECK_PROGS(MAKE,make gnumake nmake pmake smake)\nfi' contrib/hwloc/configure.ac
-fi
 
-# Prevent top_srcdir from getting broken in contrib/hwloc by not using recursive autoreconf
+# Prevent the following issues associated with recursive autoreconf
+# (1) top_srcdir in am__aclocal_m4_deps in contrib/hwloc/Makefile.in is broken
+# (2) AC_SUBST([MAKE]) is missing in contrib/hwloc/configure.ac.
+#     Do the following if you want to fix it manually.
+#     if ! grep -qE 'AC_CHECK_PROGS\(MAKE,make gnumake nmake pmake smake\)' contrib/hwloc/configure.ac; then
+#         sed -i '/^AM_INIT_AUTOMAKE/ a if test "X$MAKE" = "X" ; then\n\tAC_CHECK_PROGS(MAKE,make gnumake nmake pmake smake)\nfi' contrib/hwloc/configure.ac
+#     fi
 subdirs=". contrib/hwloc"
 for subdir in $subdirs; do
     (cd $subdir && autoreconf -iv --no-recursive)
